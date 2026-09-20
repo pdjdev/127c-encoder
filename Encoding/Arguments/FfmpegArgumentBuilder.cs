e41884e6cc8daff4c384153409bacb825770be9f
Dongjun Park <pdj5096@gmail.com>
2026-09-20 20:09:14 +0900

    Add FFmpeg stages for fdkaac pipeline

--- Encoding/Arguments/FfmpegArgumentBuilder.cs
+++ Encoding/Arguments/FfmpegArgumentBuilder.cs
@@ -5,6 +5,9 @@ namespace Encoder127c.Encoding.Arguments;
 internal interface IFfmpegArgumentBuilder
 {
     IEnumerable<string> Build(ValidatedVideoEncodingRequest request);
+    IEnumerable<string> BuildVideoOnly(ValidatedVideoEncodingRequest request, string outputPath);
+    IEnumerable<string> BuildAudioPipe(ValidatedVideoEncodingRequest request);
+    IEnumerable<string> BuildRemux(string videoPath, string audioPath, string outputPath);
 }
 
 /// <summary>Translates a validated application request into FFmpeg CLI arguments.</summary>
@@ -45,6 +48,66 @@ internal sealed class FfmpegArgumentBuilder : IFfmpegArgumentBuilder
             request.OutputPath
         ];
     }
+
+    public IEnumerable<string> BuildVideoOnly(ValidatedVideoEncodingRequest request, string outputPath)
+    {
+        var isSavingProfile = request.EncodingProfile == DefaultEncodingPreset.EncodingProfileSaving;
+
+        return
+        [
+            "-hide_banner",
+            "-nostats",
+            "-n",
+            "-i", request.InputPath,
+            "-map", "0:v:0",
+            "-an",
+            "-c:v", DefaultEncodingPreset.VideoCodec,
+            "-preset", request.VideoPreset,
+            "-tune", DefaultEncodingPreset.VideoTune,
+            "-profile:v", DefaultEncodingPreset.VideoProfile,
+            "-level:v", DefaultEncodingPreset.VideoLevel,
+            "-crf", isSavingProfile ? DefaultEncodingPreset.SavingVideoCrf : DefaultEncodingPreset.VideoCrf,
+            "-maxrate", request.VideoMaxBitrate,
+            "-bufsize", request.VideoBufferSize,
+            "-pix_fmt", "yuv420p",
+            "-fps_mode", "vfr",
+            "-movflags", "+faststart",
+            "-metadata", "encoder=127c-encoder",
+            "-progress", "pipe:1",
+            .. BuildVideoFilterArguments(request, isSavingProfile),
+            outputPath
+        ];
+    }
+
+    public IEnumerable<string> BuildAudioPipe(ValidatedVideoEncodingRequest request) =>
+    [
+        "-hide_banner",
+        "-nostats",
+        "-v", "warning",
+        "-i", request.InputPath,
+        "-map", "0:a:0?",
+        "-vn",
+        "-ac", DefaultEncodingPreset.AudioChannels,
+        "-af", BuildAudioFilter(request),
+        "-c:a", "pcm_s16le",
+        "-f", "caf",
+        "pipe:1"
+    ];
+
+    public IEnumerable<string> BuildRemux(string videoPath, string audioPath, string outputPath) =>
+    [
+        "-hide_banner",
+        "-nostats",
+        "-n",
+        "-i", videoPath,
+        "-i", audioPath,
+        "-map", "0:v:0",
+        "-map", "1:a:0",
+        "-c", "copy",
+        "-movflags", "+faststart",
+        "-metadata", "encoder=127c-encoder",
+        outputPath
+    ];
 
     private static IEnumerable<string> BuildVideoFilterArguments(ValidatedVideoEncodingRequest request, bool isSavingProfile)
     {
